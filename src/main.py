9f8587327b89d83cac5a901e9ee838082713fae4
Daniel Rajakumar <danielelvisr@gmail.com>
2023-12-13 17:24:39 -0500

    Refactor data loading and plotting code

--- src/main.py
+++ src/main.py
@@ -1,51 +1,46 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-# get data from url
-data_url = "https://raw.githubusercontent.com/daniel-rajakumar/CMPS240_FinalProject/main/res/iPhone.csv"
-sales_data = pd.read_csv(data_url)
+# load data from remote csv
+url = "https://raw.githubusercontent.com/daniel-rajakumar/CMPS240_FinalProject/main/res/iPhone.csv"
+sales_data = pd.read_csv(url)
 
-# make year as Y-axis
+# Calculate growth rates for each region
+regions = sales_data.columns[1:]  # remove Year
+growth_data = pd.DataFrame()  
+for region in regions:
+    growth_data[f'{region}_Growth'] = sales_data[region].pct_change() * 100
+
+# set 'Year' column as index
 sales_data.set_index('Year', inplace=True)
 
-# growth rate
-sales_data_growth = sales_data.pct_change() * 100  # Calculate percentage change
+# Bar chart for sales data
+plt.subplot(1, 2, 1)  # relocate the chart
+sales_data[regions].plot(kind='bar', ax=plt.gca())
+plt.title('iPhone Sales in Different Regions Over 5 Years')
+plt.xlabel('Year')
+plt.ylabel('Sales (mm)')
 
-#######################
-###### Plotting ######
-######################
-
-fig, ax1 = plt.subplots(figsize=(10, 6))  # Create a figure and axes
-ax2 = ax1.twinx()  # Create a second y-axis sharing the same x-axis
-
-# set up plot for sale
-for column in sales_data.columns:
-    ax1.plot(sales_data.index, sales_data[column], label=f'{column} Sales', linestyle='-', marker='o')
-
-# set up plot for growth rage
-for column in sales_data_growth.columns:
-    ax2.plot(sales_data_growth.index, sales_data_growth[column], label=f'{column} Growth Rate', linestyle='--', marker='x')
-
-# Set labels and title for the plot
-ax1.set_xlabel('Year')
-ax1.set_ylabel('iPhone Sales')
-ax2.set_ylabel('Growth Rate (%)')
-ax1.set_title('iPhone Sales and Growth Rate in Different Regions Over Time')
-
-# Show legends
-lines1, labels1 = ax1.get_legend_handles_labels()
-lines2, labels2 = ax2.get_legend_handles_labels()
-ax1.legend(lines1 + lines2, labels1 + labels2, loc='upper left')
-
+# add avg sale (%) for each region on bottom of sale graph
 note_text = "Average Growth Rate for Each Region:"
-for region, growth_rate in sales_data_growth.items():
-    note_text += f"\n{region}: {growth_rate.mean():.2}%"
+for region, sales_rate in sales_data.items():
+    note_text += f"\n{region}: {sales_rate.mean():.4}%"
+plt.annotate(note_text, xy=(0.5, -0.3), xycoords='axes fraction', ha='center', fontsize=10, color='gray', wrap=True)
 
+# Bar chart for growth rates
+plt.subplot(1, 2, 2)  # relocate the chart
+growth_data.plot(kind='bar', ax=plt.gca())
+plt.title('iPhone Sales Growth Rate in Different Regions Over 5 Years')
+plt.xlabel('Year')
+plt.ylabel('Growth Rate (%)')
+
+# add avg growth rate (%) for each region on bottom of growth graph
+note_text = "Average Growth Rate for Each Region:"
+for region, sales_rate in growth_data.items():
+    note_text += f"\n{region}: {sales_rate.mean():.3}%"
 plt.annotate(note_text, xy=(0.5, -0.3), xycoords='axes fraction', ha='center', fontsize=10, color='gray', wrap=True)
 
 
-# Display the plot
-plt.grid(True)
-plt.tight_layout()
+# Show plot
+plt.tight_layout()  # Adjust layout
 plt.show()
-
